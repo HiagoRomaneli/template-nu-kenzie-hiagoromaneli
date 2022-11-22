ff92706aba22a60482edd5ec689e24c122a48e5c
HiagoRomaneli <hi_agorodrigues@hotmail.com>
2022-11-22 17:51:18 -0300

    feat: border nas transaçoes de entrada

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,21 @@
 import "./card.css";
 
 export const Card = ({ transaction, deleteTransaction }) => {
+  if (transaction.type === "entrada") {
+    return (
+      <div className="div-card entry-transaction">
+        <div>
+          <h3>{transaction.description}</h3>
+          <p>{transaction.type}</p>
+        </div>
+        <div>
+          <p>R$ {transaction.value},00</p>
+          <button onClick={() => deleteTransaction(transaction)}></button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="div-card">
       <div>
